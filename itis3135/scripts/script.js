27470ff2b4bb8f2cf6ac5b99c17e0cf0e2bf7fa0
AliA27701 <aalimi2005@gmail.com>
2025-05-06 14:11:26 -0400

    hi

--- itis3135/scripts/script.js
+++ itis3135/scripts/script.js
@@ -1,23 +1,7 @@
-function sendMail(event) {
-    event.preventDefault();
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
-    };
+document.addEventListener("DOMContentLoaded", function () {
+    const ef = document.getElementById("contact-form");
 
-    console.log("Attempting to send email with:");
-    console.log("Service ID: service_20g5nn8");
-    console.log("Template ID: template_wc1nwyq");
-    console.log("Parameters:", parms);
+    ef.setAttribute("action", "https://formsubmit.co/scrummasterdz@gmail.com");
+    ef.setAttribute("method", "POST");
 
-    emailjs.send("service_20g5nn8", "template_wc1nwyq", parms)
-        .then(function(response) {
-            console.log("WIN", response.status, response.text);
-            alert("Email Sent");
-        }, function(error) {
-            alert("Email failed to send. Check console.");
-            console.error("EMAIL ERROR:", error);
-        });
-}
+});
