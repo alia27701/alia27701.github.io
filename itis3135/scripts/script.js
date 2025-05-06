614e1e6f1b5ff6549545e0aadbfaaf6e1a7a8edf
AliA27701 <aalimi2005@gmail.com>
2025-05-06 03:27:42 -0400

    h

--- itis3135/scripts/script.js
+++ itis3135/scripts/script.js
@@ -1,19 +1,23 @@
-function sendEmail() {
-    const templateParams = {
-        name: document.querySelector("#name").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value
+function sendMail(event) {
+    event.preventDefault();
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
     };
 
-    emailjs
-    .send("DanScrumMaster", "template_5dzx49q", templateParams)
-    .then(() => {
-        alert("Email Sent!!");
-        document.getElementById("contact-form").reset();
-    })
-    .catch((error) => {
-        alert("Email not sent");
-        console.error(error);
-    });
+    console.log("Attempting to send email with:");
+    console.log("Service ID: service_20g5nn8");
+    console.log("Template ID: template_wc1nwyq");
+    console.log("Parameters:", parms);
+
+    emailjs.send("service_20g5nn8", "template_wc1nwyq", parms)
+        .then(function(response) {
+            console.log("WIN", response.status, response.text);
+            alert("Email Sent");
+        }, function(error) {
+            alert("Email failed to send. Check console.");
+            console.error("EMAIL ERROR:", error);
+        });
 }
